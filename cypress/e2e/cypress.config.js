f7a349afd068307877aa5b5d19643f7f1954bd74
yhjin-QA <yhjin@warevalley.com>
2025-12-29 17:25:55 +0900

    Use self-hosted runner

--- cypress/e2e/cypress.config.js
+++ cypress/e2e/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
-  projectId: "7f28e0a4-1dd8-45cd-aed7-e261939654e9",
+  projectId: "7yuixr",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
