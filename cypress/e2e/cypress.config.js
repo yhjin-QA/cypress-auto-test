df6ad0da7e64bf7933d446a8d09cf0d3c90283e8
yhjin-QA <yhjin@warevalley.com>
2025-12-29 16:48:02 +0900

    Add projectId to config

--- cypress/e2e/cypress.config.js
+++ cypress/e2e/cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  projectId: "7f28e0a4-1dd8-45cd-aed7-e261939654e9",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
