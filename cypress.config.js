2f74e8bfd92d821acb45e31f90c9ceae130e89cc
yhjin-QA <yhjin@warevalley.com>
2025-12-30 11:05:50 +0900

    Exclude specific folders from tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "7yuixr",
   e2e: {
-      chromeWebSecurity: false,
+
+    // ▼▼▼ [수정] 아래 부분을 추가하세요 ▼▼▼
+    excludeSpecPattern: [
+      "**/UI 체크/**",          // 'UI 체크' 폴더 안의 모든 것 제외
+    ], // ▲▲▲ [여기까지] ▲▲▲
+    
+    chromeWebSecurity: false,
     viewportWidth: 1600,
     viewportHeight: 900,
     setupNodeEvents(on, config) {
