d945e4e512976562c5a5d3bf567feb9d78b204de
yhjin-QA <yhjin@warevalley.com>
2026-04-22 18:04:14 +0900

    add depth 검출 필터 식별자패턴 code3

--- cypress/e2e/Depth_functions_check/02_Depth_이력_사용자추적_spec.cy.js
+++ cypress/e2e/Depth_functions_check/02_Depth_이력_사용자추적_spec.cy.js
@@ -333,7 +333,7 @@ describe('로그캐치 사이트 테스트', () => {
     //  });
      //---------------------------------------------------------------------------------------------------------------
 
-     // 부서 : 경영지원팀  -------------------------------------------------------------------------------------------
+     // 부서 : 영업팀  -------------------------------------------------------------------------------------------
     // 2번째부터는 선택 초기화 필요 
     // 선택한 부서/소속 x버튼 클릭하여 초기화 
     cy.get('input[aria-label="부서/소속"]').filter(':visible').closest('.v-input').find('.v-input__icon--clear').find('.v-icon').click({ force: true });
@@ -343,8 +343,8 @@ describe('로그캐치 사이트 테스트', () => {
     cy.get('input[aria-label="부서/소속"]').filter(':visible').click({ force: true });
     cy.wait(500);
 
-    // 리스트에서 '경영지원팀'가 나타날 때까지 기다리고 클릭
-    cy.get('.v-menu__content').filter(':visible').contains('.v-list__tile__title', '경영지원팀', { timeout: 10000 }).should('be.visible').click({ force: true });
+    // 리스트에서 '영업팀'가 나타날 때까지 기다리고 클릭
+    cy.get('.v-menu__content').filter(':visible').contains('.v-list__tile__title', '영업팀', { timeout: 10000 }).should('be.visible').click({ force: true });
     cy.wait(500);
     // 3. 선택 후 메뉴 닫기 (필요시)
     cy.get('body').type('{esc}');
@@ -354,11 +354,11 @@ describe('로그캐치 사이트 테스트', () => {
     cy.wait(1000);
 
     // [검증] 검색 결과 검증
-    cy.get('tbody tr', { timeout: 10000 }).contains('경영지원팀').should('be.visible');
+    cy.get('tbody tr', { timeout: 10000 }).contains('영업팀').should('be.visible');
 
     // 첫 번째 행 정밀 검증
     cy.get('tbody tr').filter(':visible').first().within(() => {
-    cy.get('a').contains('경영지원팀').should('be.visible').and('have.css', 'color', 'rgb(0, 0, 0)');
+    cy.get('a').contains('영업팀').should('be.visible').and('have.css', 'color', 'rgb(0, 0, 0)');
      });
      //---------------------------------------------------------------------------------------------------------------
 
