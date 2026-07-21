d467fb17a942522b99ed37efc22a4d61280bc518
yhjin <your_email@example.com>
2026-07-21 17:11:19 +0900

    Add Squish test suite for ChakraMax automation

---
+++ suite_TEST_QA/tst_test_chakramax_Client/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import names
+
+
+def main():
+    startApplication("MaxRunner")
+    type(waitForObject(names.o_Edit), "Manager1!@")
+    type(waitForObject(names.o_Edit), "<Return>")
+   
+    mouseClick(waitForObject(names.o_TabItem_2))
+    mouseClick(waitForObject(names.o_TabItem))
+    mouseClick(waitForObject(names.o_TabItem_6))
+    mouseClick(waitForObject(names.o_TabItem_4))
+    mouseClick(waitForObject(names.o_TabItem_5))
+    mouseClick(waitForObject(names.o_TabItem_3))
